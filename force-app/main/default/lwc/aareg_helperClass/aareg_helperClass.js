e4ede4dd47d293362d53e50bfb5cf629c4e04967
fluidfoslex <alexander.fosse@fluidogroup.com>
2024-05-15 14:56:17 +0200

    Refreshed repo

--- force-app/main/default/lwc/aareg_helperClass/aareg_helperClass.js
+++
@@ -1,7 +0,0 @@
-export function validateEmail(email) {
-  const regExp = /\S+@\S+\.\S+/;
-    if (!regExp.test(email) || email === null || email === '') {
-      return true;
-    } 
-    return false;
-  }
--- force-app/main/default/lwc/aareg_missingAccess/aareg_missingAccess.js
+++
@@ -1,3 +0,0 @@
-import { LightningElement } from 'lwc';
-
-export default class Aareg_missingAccess extends LightningElement {}
