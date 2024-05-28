9fd357c430fb35747aa3475eb289c97ac876b558
FluidoPhilip <167406253+FluidoPhilip@users.noreply.github.com>
2024-05-28 14:21:17 +0200

    Removed metadata

--- force-app/main/default/lwc/essoHeader/essoHeader.js
+++
@@ -1,6 +0,0 @@
-import { LightningElement } from 'lwc';
-import ESSO_LOGO from '@salesforce/resourceUrl/essoLogo';
-
-export default class EssoHeader extends LightningElement {
-    essoLogo = ESSO_LOGO;
-}
